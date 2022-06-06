fb808efc95ddda4255f7566c308d48fdb4720611
Steve Malers <steve.malers@openwaterfoundation.org>
2022-06-06 13:01:31 -0600

    Add the initial AwsCloudFront command, issue #1.

--- owf-tstool-aws-plugin/src/main/java/org/openwaterfoundation/tstool/plugin/aws/AwsToolkit.java
+++ owf-tstool-aws-plugin/src/main/java/org/openwaterfoundation/tstool/plugin/aws/AwsToolkit.java
@@ -31,6 +31,13 @@ import RTi.Util.IO.IOUtil;
 import RTi.Util.Message.Message;
 import software.amazon.awssdk.auth.credentials.ProfileCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.cloudfront.CloudFrontClient;
+import software.amazon.awssdk.services.cloudfront.model.DistributionSummary;
+import software.amazon.awssdk.services.cloudfront.model.InvalidationSummary;
+import software.amazon.awssdk.services.cloudfront.model.ListDistributionsRequest;
+import software.amazon.awssdk.services.cloudfront.model.ListDistributionsResponse;
+import software.amazon.awssdk.services.cloudfront.model.ListInvalidationsRequest;
+import software.amazon.awssdk.services.cloudfront.model.ListInvalidationsResponse;
 import software.amazon.awssdk.services.s3.S3Client;
 import software.amazon.awssdk.services.s3.model.Bucket;
 import software.amazon.awssdk.services.s3.model.ListBucketsRequest;
@@ -48,33 +55,99 @@ public class AwsToolkit {
 	public static AwsToolkit getInstance() {
 		return instance;
 	}
+	
+	// -----------------------------------------------------------------------------
 
 	/**
- 	* Get the list of buckets.
- 	*/
-	public List<Bucket> getBuckets ( AwsSession awsSession, String region ) {
+	 * Get the CloudFront distribution ID to use for CloudFront commands.
+  	 * @param awsSession the AWS session, containing profile
+ 	 * @param region the AWS region
+	 * @param distributionId if not null and not empty, the distribution ID to use, takes precedence over the comment
+	 * @param commentPattern if not null and not empty, a Java pattern to match the distribution comment
+	 * @return the distribution ID to use after evaluating input, or null if not matched.
+	 */
+   	public String getCloudFrontDistributionId( AwsSession awsSession, String region, String distributionId, String commentPattern ) {
+   		// Get the list of distributions.
+   		List<DistributionSummary> distributions = getCloudFrontDistributions ( awsSession, region );
+   		// Match a specific distribution.
+   		for ( DistributionSummary distribution : distributions ) {
+   			if ( (distributionId != null) && !distributionId.isEmpty() && distribution.id().equals(distributionId) ) {
+   				return distributionId;
+   			}
+   			else if ( (commentPattern != null) && !commentPattern.isEmpty() && distribution.comment().matches(commentPattern) ) {
+   				return distribution.id();
+   			}
+   		}
+   		// No match, return null.
+   		return null;
+   	}
+
+	/**
+ 	 * Get the list of CloudFront distributions.
+ 	 * @param awsSession the AWS session, containing profile
+ 	 * @param region the AWS region
+ 	 * @return a list of DistributionSummary or an empty list if no distributions
+ 	 */
+	public List<DistributionSummary> getCloudFrontDistributions ( AwsSession awsSession, String region ) {
 		ProfileCredentialsProvider credentialsProvider = null;
 		String profile = awsSession.getProfile();
+		// If the region is not specified, use the default.
+		if ( (region == null) || region.isEmpty() ) {
+			region = getDefaultRegion ( profile );
+		}
 		try {
 			credentialsProvider = ProfileCredentialsProvider.create(profile);
 			Region regionObject = Region.of(region);
-			S3Client s3 = S3Client.builder()
+			CloudFrontClient cloudfront = CloudFrontClient.builder()
 				.region(regionObject)
 				.credentialsProvider(credentialsProvider)
 				.build();
-    		ListBucketsRequest request = ListBucketsRequest.builder().build();
-    		ListBucketsResponse response = s3.listBuckets(request);
-    		return response.buckets();
+    		ListDistributionsRequest request = ListDistributionsRequest.builder().build();
+    		ListDistributionsResponse response = cloudfront.listDistributions(request);
+    		return response.distributionList().items();
 		}
 		catch ( Exception e ) {
 			// Log the error and return an empty list:
 			// - may have requested an invalid region
-			String routine = getClass().getSimpleName() + ".getBuckets";
-			Message.printWarning(3, routine, "Error getting list of buckets (" + e + ").");
+			String routine = getClass().getSimpleName() + ".getCloudFrontDistributions";
+			Message.printWarning(3, routine, "Error getting list of distributions (" + e + ").");
 		}
-		return new ArrayList<Bucket>();
+		return new ArrayList<DistributionSummary>();
 	}
-	
+
+	/**
+ 	 * Get the list of CloudFront invalidations.
+ 	 * @param awsSession the AWS session, containing profile
+ 	 * @param region the AWS region
+ 	 * @return a list of InvalidationSummary or an empty list if no invalidations
+ 	 */
+	public List<InvalidationSummary> getCloudFrontInvalidations ( AwsSession awsSession, String region ) {
+		ProfileCredentialsProvider credentialsProvider = null;
+		String profile = awsSession.getProfile();
+		// If the region is not specified, use the default.
+		if ( (region == null) || region.isEmpty() ) {
+			region = getDefaultRegion ( profile );
+		}
+		try {
+			credentialsProvider = ProfileCredentialsProvider.create(profile);
+			Region regionObject = Region.of(region);
+			CloudFrontClient cloudfront = CloudFrontClient.builder()
+				.region(regionObject)
+				.credentialsProvider(credentialsProvider)
+				.build();
+    		ListInvalidationsRequest request = ListInvalidationsRequest.builder().build();
+    		ListInvalidationsResponse response = cloudfront.listInvalidations(request);
+    		return response.invalidationList().items();
+		}
+		catch ( Exception e ) {
+			// Log the error and return an empty list:
+			// - may have requested an invalid region
+			String routine = getClass().getSimpleName() + ".getCloudFrontInvalidations";
+			Message.printWarning(3, routine, "Error getting list of invalidations (" + e + ").");
+		}
+		return new ArrayList<InvalidationSummary>();
+	}
+
 	/**
 	 * Determine the default profile, typically called when code has not specified a profile.
 	 * @return the default profile to use, which will be the only profile if one exists,
@@ -101,6 +174,82 @@ public class AwsToolkit {
 			// No profile named 'default' so can't determine the default profile.
 			return null;
 		}
+	}
+
+	/**
+	 * Get the default region from the ~/.config file entry for the specified profile.
+	 * See: https://docs.aws.amazon.com/cli/latest/userguide/cli-configure-profiles.html
+	 * See: https://docs.aws.amazon.com/cli/latest/userguide/cli-configure-quickstart.html#cli-configure-quickstart-region
+	 * 
+	 * The file will have something like the following if the region is set:
+	 * 
+	 * [profile profile-name]
+	 * region = us-west-2
+	 * output = text
+	 * 
+	 * [default]
+	 * region = us-west-2
+	 * output = text
+	 */
+	public String getDefaultRegion ( String profile ) {
+		String routine = getClass().getSimpleName() + ".getDefaultRegion";
+		String region = "";
+		// Get the user's home folder.
+		String userFolder = System.getProperty("user.home");
+		String configFileName = userFolder + File.separator + ".aws" + File.separator + "config";
+		File configFile = new File(configFileName);
+		if ( configFile.exists() ) {
+			try {
+				List<String> lines = IOUtil.fileToStringList(configFileName);
+				boolean profileFound = false;
+				for ( String line : lines ) {
+					// Profile sections have the following form:
+					// [default]
+					// [profile profileName]
+					line = line.trim();
+					if ( line.equals("[default]") ) {
+						if ( profile.equals("default") ) {
+							// Found the requested profile.
+							profileFound = true;
+						}
+						else {
+							profileFound = false;
+						}
+					}
+					else if ( line.startsWith("[profile") ) {
+						int pos = line.indexOf(" ");
+						if ( pos > 0 ) {
+							if ( profile.equals(line.substring(pos).replace("]","").trim()) ) {
+								// Found the requested profile.
+								profileFound = true;
+							}
+							else {
+								profileFound = false;
+							}
+						}
+					}
+					if ( profileFound ) {
+						// Profile was found so search for region.
+						if ( line.startsWith("region") ) {
+							int pos = line.indexOf("=");
+							if ( (pos > 0) && ((pos + 1) < line.length()) ) {
+								// Make sure the string includes value to the right of =.
+								region = line.substring(pos + 1).trim();
+								break;
+							}
+						}
+					}
+				}
+			}
+			catch ( IOException e ) {
+				Message.printWarning(3, routine, "Error processing AWS configuration file: " + configFileName);
+				Message.printWarning(3, routine, e);
+			}
+		}
+		else {
+			Message.printWarning(3, routine, "AWS configuration file does not exist: " + configFileName);
+		}
+		return region;
 	}
 	
 	/**
@@ -143,4 +292,33 @@ public class AwsToolkit {
 		}
 		return profiles;
 	}
+
+	/**
+ 	* Get the list of S3 buckets.
+ 	* @param awsSession the AWS session, containing profile
+ 	* @param region the AWS region
+ 	*/
+	public List<Bucket> getS3Buckets ( AwsSession awsSession, String region ) {
+		ProfileCredentialsProvider credentialsProvider = null;
+		String profile = awsSession.getProfile();
+		try {
+			credentialsProvider = ProfileCredentialsProvider.create(profile);
+			Region regionObject = Region.of(region);
+			S3Client s3 = S3Client.builder()
+				.region(regionObject)
+				.credentialsProvider(credentialsProvider)
+				.build();
+    		ListBucketsRequest request = ListBucketsRequest.builder().build();
+    		ListBucketsResponse response = s3.listBuckets(request);
+    		return response.buckets();
+		}
+		catch ( Exception e ) {
+			// Log the error and return an empty list:
+			// - may have requested an invalid region
+			String routine = getClass().getSimpleName() + ".getS3Buckets";
+			Message.printWarning(3, routine, "Error getting list of buckets (" + e + ").");
+		}
+		return new ArrayList<Bucket>();
+	}
+	
 }
