36840f256ffee6d40eceb4bbd904d581310461cd
Steve Malers <steve.malers@openwaterfoundation.org>
2024-10-08 09:06:51 -0600

    Fix bug with VPN and ElasticIP tags not being handled, issue #50

--- owf-tstool-aws-plugin/src/main/java/org/openwaterfoundation/tstool/plugin/aws/PluginMeta.java
+++ owf-tstool-aws-plugin/src/main/java/org/openwaterfoundation/tstool/plugin/aws/PluginMeta.java
@@ -26,7 +26,7 @@ public class PluginMeta {
 	/**
 	 * Plugin version.
 	 */
-	public static final String VERSION = "1.5.3 (2024-10-04)";
+	public static final String VERSION = "1.5.4 (2024-10-08)";
 	
 	/**
 	 * Get the documentation root URL, used for command help.
